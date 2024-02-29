db656a4107cafdd684cfc3bb023bc0f1ca6d13be
jombaa <jombaa00@gmail.com>
2024-02-29 16:08:53 +0100

    fikset en skrivefeil

--- src/main/java/oslomet/testing/DAL/AdminRepository.java
+++ src/main/java/oslomet/testing/DAL/AdminRepository.java
@@ -165,7 +165,7 @@ public class AdminRepository {
             db.update(sql,kontonummer);
         }
         catch(Exception e){
-            return "Feil kononummer";
+            return "Feil kontonummer";
         }
         return "OK";
     }
