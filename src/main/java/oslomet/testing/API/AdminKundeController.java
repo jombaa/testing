cd3acf1889257fbe5503b5cfa51ac096d5514784
jombaa <jombaa00@gmail.com>
2024-03-07 12:55:58 +0100

    Fjernet en tom linje

--- src/main/java/oslomet/testing/API/AdminKundeController.java
+++ src/main/java/oslomet/testing/API/AdminKundeController.java
@@ -11,8 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/adminKunde")
-public class
-AdminKundeController {
+public class AdminKundeController {
     @Autowired
     AdminRepository repository;
 
